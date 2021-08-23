4d3c6dfad18c6a116da01cb04e8865fc7584b17d
peter coates <coatespt@gmail.com>
2021-08-23 13:58:12 -0400

    Commented out a lot of tests.

--- src/test/java/com/levenshtein/MasterTest.java
+++ src/test/java/com/levenshtein/MasterTest.java
@@ -9,7 +9,17 @@ import org.junit.runners.Suite;
  * TODO: find out why the RollingHash signatures seem to be slightly larger than the plain has signatures. Could explain the performance difference.
  * TODO  Get material for a graph on actual compute time for files of increasing size 
  * TODO: fix the compression routines so that they always adjust to an odd number or to one of the good numbers;.
- *  
+ * TODO: put all compression tests in one file.
+ * 			* RH		Compare actual is close to nominal
+ * 		  	* Regular	Compare actual is close to nominal
+ * 		  	* Both		Count occurences of caracters in the output. They should be pretty close.
+ * 		    * Comparison: Do them side by side and ensure that they compress roughly equally
+ * 			* Comparison: Compare the LD results. They should be rougly equal
+ *			* Comparison: How does speed compare for the same settings.
+ *
+ *
+ *
+ *
  * @author peter
  * fudge() isn't called anywhere except the TestMassCompare 
  * 
@@ -20,14 +30,14 @@ import org.junit.runners.Suite;
 			TestAccuracyDriverPlain101.class,
 			TestAccuracyDriverRH101.class,
 			TestBasicOperations.class,
-			TestCompareAccuracy.class,
-			//TestMassCompare.class,
-			TestRollingHash.class,
-			TestPlainWRangeOfCAndN.class,
+			//  TestCompareAccuracy.class,
+			//  TestMassCompare.class,
+			//TestRollingHash.class,
+			//TestPlainWRangeOfCAndN.class,
 			TestRegularHash.class,
-			TestRollingHash.class,
-			TestSpeedForBigFiles.class,
-			TestRegularVRolling101.class,
+			//  TestSpeedForBigFiles.class,
+			//	TestVaryCandN.class,
+			//  TestRegularVRolling101.class,
 			}
 	)
 	public class MasterTest {
